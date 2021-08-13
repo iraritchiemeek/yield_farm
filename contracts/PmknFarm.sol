270a41a25f2184c6e8b3b535b19b93f1a0663dd4
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2021-08-14 09:34:46 +1200

    stake fn

--- contracts/PmknFarm.sol
+++ contracts/PmknFarm.sol
@@ -24,8 +24,24 @@ contract PmknFarm {
 		pmknToken = _pmknToken;
 	}
 
-	stake() public {}
-	unstake() public {}
-	yieldWithdraw() public {}
+	function stake(uint256 amount) public {
+		require(amount > 0 && daiToken.balanceOf(msg.sender) >= amount, "You cannot stake zero tokens");
+
+		if (isStaking[msg.sender] == true) {
+			uint256 toTransfer = calculateYieldTotal(msg.sender);
+			pmknBalance[msg.sender] += toTransfer;
+		}
+
+		daiToken.transferFrom(msg.sender, address(this), amount);
+		stakingBalance[msg.sender] += amount;
+		startTime[msg.sender] = block.timestamp;
+		isStaking[msg.sender] = true;
+		emit Stake(msg.sender, amnount);
+	}
 
 }
+
+
+
+
+
